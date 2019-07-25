5433b5c09d62228676083b51219aefa9553838d0
KFad <kfadigan@yahoo.com>
2019-07-25 10:02:57 -0400

    Practice using container components

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,19 @@
-// Code MovieReviews Here
+import React from 'react';
+
+const Review = ({ headline, link }) => (
+  <div className="review">
+    <a className="review-link" href={link.url}>{headline}</a>
+  </div>
+)
+
+const ReviewList = ({ reviews }) => (
+  <div className="review-list">
+    { reviews.map(Review) }
+  </div>
+)
+
+  ReviewList.defaultProps = {
+    reviews: []
+  };
+
+export default ReviewList
